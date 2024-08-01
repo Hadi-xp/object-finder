6b25255ead16defd5cb9aafa0e42fc4b9af7bd34
Hadixp <hadi.b.2002@gmail.com>
2024-08-02 02:09:03 +0430

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,6 @@ if (myString.includes(target) == true) {
 } else {
   console.log(`${target} is not inside myString!`);
 }
+
+
+// as you can see if my condition be true it will log the first statment and if it wont the second statment shows up!!
