20fc95d042f48de4219077574f955aeb65d4d663
Hadixp <hadi.b.2002@gmail.com>
2024-08-02 01:57:13 +0430

    added: html and js codes

---
+++ main.js
@@ -0,0 +1,12 @@
+// this file includes a simple if for finding a word in another string var
+
+const myString = "#zan_zendegi_azadi";
+const target = "azadi";
+
+// this is a simple if() i used includes mrthod for this !!!
+
+if (myString.includes(target) == true) {
+  console.log("Are baba !!!");
+} else {
+  console.log("gashtam nabood nagard nist");
+}
